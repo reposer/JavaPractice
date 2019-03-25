5e8ed6464185b5af075d69c1a8a40503f0ee2883
jangseunghun <seunghun.jang90@gmail.com>
2019-03-25 23:40:05 +0900

    variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,20 @@
+/*
+    Date: 2019-03-25
+    Author: Jang
+    Description: My First Java Program
+    Version: 1.0
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, Java");
+
+        //System.out.println("Hello, Java"); 한 줄 주석 연습
+
+        int level;
+        level = 10;
+
+        System.out.println(level);
+
     }
 }
